d7bc4057d405d5f8c2d885f7db5780f3ec2f0a96
Tahmid Zaman <tahmid198@yahoo.com>
2023-01-27 20:52:02 -0500

    added new pages for routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
+// import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
