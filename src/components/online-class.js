7750802ddbccee95214f43fbe0c1969f96920cad
Tahmid Zaman <tahmid198@yahoo.com>
2023-01-29 23:51:38 -0500

    online class page inital layout

--- src/components/online-class.js
+++ src/components/online-class.js
@@ -1,3 +1,22 @@
+import React from 'react';
+import  './css/online-class.css';
+
 export default function OnlineClass(){
-    return <h1>Online Class</h1>
+    return (
+        <section >
+            <div className='title'>
+                <div className='title-info'>
+                    <h1>Online Classes</h1>
+                        <p>Summer 2023</p>
+                </div>
+            </div>
+
+            <div className='schedule'>
+                <div className='schedule-info'>
+                    <h1>SUMMER 2023 SCHEDULE</h1>
+                </div>
+            </div>
+        </section>
+        
+    )
 }
