49aba6a5bf8150339327ba244224d4f4a2561c91
Tahmid Zaman <tahmid198@yahoo.com>
2023-02-17 16:21:57 -0500

    footer with links to facebook and insta

---
+++ src/components/footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './css/footer.css';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faYoutube,
+    faFacebook,
+    faTwitter,
+    faInstagram
+  } from "@fortawesome/free-brands-svg-icons";
+
+export default function Footer() {
+    return (
+        <div className='social-container'>
+        <h2>&copy; Rawdah Institute</h2>
+        <a href="https://www.instagram.com/rawdahinstitute/?hl=en" className="instagram social">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="https://www.facebook.com/people/Rawdah-Institute/100057197180285/" className="facebook social">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+        </div>    
+    )
+}
