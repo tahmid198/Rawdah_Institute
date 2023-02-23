9bfd4ae602ff89bd084aff3200f9e05f810cdfb1
redwankhan <redwankhan144@gmail.com>
2023-02-22 19:11:18 -0500

    updated homepage

--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,38 @@
+import './css/home.css';
+import { Link } from 'react-router-dom';
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+
+<section className="home-container">
+
+    <div className="background">
+
+    </div>
+
+    <div className="home">
+        <div className="home-info">
+<h1>Welcome to Rawdah Institute</h1>
+<h2>“Whoever takes a path upon which to obtain knowledge, Allah makes the path to Paradise easy for him.”
+
+— Jami` at-Tirmidhi 2646</h2>
+<nav>
+    <ul>
+        <li><Link to="/OnlineClass"></Link></li>
+    </ul>
+</nav>
+<h3>
+
+<li><Link to="/OnlineClass">Click to see the latest classes we have to offer for Spring 2023</Link></li>
+
+</h3>
+        </div>
+
+    </div>
+
+</section>
+
+
+
+      
+    )
 }
