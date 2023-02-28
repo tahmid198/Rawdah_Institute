a3a55cb53c0acb7dbfc2314c75b2181012cf7afa
redwankhan <redwankhan144@gmail.com>
2023-02-28 03:18:47 -0500

    clean up code

--- src/components/home.js
+++ src/components/home.js
@@ -5,42 +5,37 @@ export default function Home() {
 
 <section className="home-container">
 
-    <div className="background">
-
-    </div>
 
     <div className="home">
+
         <div className="home-info">
 <h1>Welcome to Rawdah Institute</h1>
 <h2>“Whoever takes a path upon which to obtain knowledge, Allah makes the path to Paradise easy for him.”
 
-— Jami` at-Tirmidhi 2646</h2>
+— Jami` at-Tirmidhi 2646
+
+
+</h2>
+
+
 
     
         {/* <Link to="/OnlineClass"></Link> */}
     
 
         
-<h3 style={{ textDecoration: 'underline'}}>
-Check Out Our Latest Courses for:
+<h3 style={{fontSize: "24px" }}>
+Sign Up For Our Latest Courses Here:
         
 </h3>    
 
 
-
-
 <Link to="/OnlineClass">
-    
-    
-       
-
-       
-        <button style={{fontSize: '1em', padding:'5px', backgroundColor: '#F5F5F5', fontWeight: 'bold', cursor:'pointer', borderRadius: '5px', outline: 'none'}}>
+        <button style=
+        {{fontSize: '1.4em', padding:'10px 20px', backgroundColor: '#F5F5F5', fontWeight: 'bold', cursor:'pointer',
+         borderRadius: '5px', outline: 'none'}}>
    Spring 2023
         </button>
-         
-      
-        
         </Link>
        
 
