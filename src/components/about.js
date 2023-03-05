9c5c2b314603e6fad2697c13b6539bfe46c95c84
redwankhan <redwankhan144@gmail.com>
2023-03-05 17:41:25 -0500

    updated homepage and about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,3 +1,31 @@
+import './css/about.css';
 export default function About() {
-    return <h1>About</h1>
+    return(
+    <section className="about-container">
+
+        <div className='about'>
+        
+        <div className='about-info'>
+        
+        {/* //About Us title */}
+        <h1 style={{fontSize: '3.5rem', color:'#fff', fontWeight: '300'}}>About Us</h1>
+
+
+
+<h2 style={{fontSize: '1.5em'}}>
+        Rawdah Institute is an educational institute that strives to connect Muslims of all walks of life with the Islamic tradition.
+</h2>
+
+
+
+<h3 style={{fontSize: "25px", padding:'7px 10px', borderRadius: '5px'}}>
+Rawdah Institute aims to provide high quality instruction on content that includes: Quranic Recitation, Arabic Language, Islamic Studies, and Self-Development.
+
+</h3>
+</div>
+</div>
+</section>
+           
+    ) 
+    
 }
--- src/components/home.js
+++ src/components/home.js
@@ -2,14 +2,14 @@ import './css/home.css';
 import { Link } from 'react-router-dom';
 export default function Home() {
     return (
-
 <section className="home-container">
-
-
     <div className="home">
 
         <div className="home-info">
 <h1>Welcome to Rawdah Institute</h1>
+
+{/* <h5 style={{fontSize: '24px'}}>Our vision for Rawdah Institute is for it to become a vibrant, state-of-the-art, world-class institute</h5> */}
+
 <h2>“Whoever takes a path upon which to obtain knowledge, Allah makes the path to Paradise easy for him.”
 
 — Jami` at-Tirmidhi 2646
@@ -17,28 +17,23 @@ export default function Home() {
 
 </h2>
 
-
-
-    
-        {/* <Link to="/OnlineClass"></Link> */}
-    
-
-        
 <h3 style={{fontSize: "24px" }}>
 Check Out Our Latest Courses for:
         
 </h3>    
 
 
+
 <Link to="/OnlineClass">
         <button style=
-        {{fontSize: '1.4em', padding:'10px 20px', backgroundColor: '#F5F5F5', fontWeight: 'bold', cursor:'pointer',
-         borderRadius: '5px', outline: 'none'}}>
-   Spring 2023
+        {{fontSize: '1.4em', padding:'7px 10px', 
+        backgroundColor: '#F5F5F5', fontWeight: 'bold', 
+        cursor:'pointer', borderRadius: '5px', outline: 'none'}}>
+             Summer 2023
         </button>
         </Link>
-       
 
+        
         </div>
 
     </div>
