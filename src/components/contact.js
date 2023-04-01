e5234149733f8f36fcebe242ea38c015a35200a3
Tahmid Zaman <tahmid198@yahoo.com>
2023-04-01 03:22:50 -0400

    Contact page updated with css

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,43 +1,110 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import  './css/contact.css';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./css/contact.css";
+import "./css/footer.css";
 
 export default function Contact() {
+  const form = useRef();
 
-   
-    const form = useRef();
-
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-    //   emailjs.sendForm('Rawdah_Contact1', 'Rawdah_Contact_Form1', form.current, 'BvLCNuBNFSSHXUGfu')
-      emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
-
-
-        .then((result) => {
-            console.log(result.text);
-            console.log("Message Sent")
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
-    return (
-        <section className="sec-container">
-            <div className="contact-container">
-                <h1>Get in touch!</h1>
-                <h3>Sign up for a class or get in touch</h3>
-                <form className='cf'ref={form} onSubmit={sendEmail}>
-                    <div className='half left cf'>
-                        <input type='text' placeholder='Name' name='from_name' required />
-                        <input type='email' placeholder='Email address' name='from_email' required />
-                        <input type='text' placeholder='Course code' name='course_code' />
-                        </div>
-                        <div className='half right cf'>
-                        <textarea name='message' type='text' placeholder='Message'></textarea>
-                    </div>
-                    <input type='submit' value='Submit' id='input-submit' />
-                </form>
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("Message Sent");
+          window.location.reload();
+        },
+        (error) => {
+          console.log(error.text);
+          console.log(
+            process.env.REACT_APP_SERVICE_ID +
+              " " +
+              process.env.REACT_APP_TEMPLATE_ID +
+              " " +
+              process.env.REACT_APP_PUBLIC_KEY
+          );
+        }
+      );
+  };
+  return (
+    <section className="sec-wrapper">
+      <div className="page-wrapper">
+        <h1 id="page-title">Send Us A Message</h1>
+        <h4 id="page-subheading">Use the form below to sign up for a class or get in touch</h4>
+        <div className="contact-wrapper">
+          <div className="grid-item1">
+            <div className="heading-container">
+              <h1 id="contact-title">CONTACT US</h1>
+              <h2 id="contact-subheading">
+                Sign up for a class or get in touch
+              </h2>
             </div>
-        </section>    
-    )
+          </div>
+          <div className="grid-item2">
+            <div className="grid-item2-container">
+              <div className="form-container">
+                <h1 id="form-title">SEND US A MESSAGE!</h1>
+                <form className="cf" ref={form} onSubmit={sendEmail}>
+                  <div className="input-row">
+                    <div className="input-group">
+                      <label>Name</label>
+                      <input
+                        type="text"
+                        placeholder="Name"
+                        name="from_name"
+                        required
+                      />
+                    </div>
+                    <div className="input-group">
+                      <label>Email</label>
+                      <input
+                        type="email"
+                        placeholder="Email address"
+                        name="from_email"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="input-row">
+                    <div className="input-group">
+                      <label>Course code</label>
+                      <input
+                        type="text"
+                        placeholder="Course code"
+                        name="course_code"
+                      />
+                    </div>
+                    <div className="input-group">
+                      <label>Phone Number</label>
+                      <input
+                        type="text"
+                        placeholder="Phone number"
+                        name="phone_number"
+                      />
+                    </div>
+                  </div>
+                  <label>Message</label>
+                  <textarea
+                    name="message"
+                    rows="5"
+                    type="text"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                  <input type="submit" value="Submit" id="input-submit" />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
