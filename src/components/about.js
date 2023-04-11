b8d58801e1256c05e7f3dada753eb61ff44491d1
redwankhan <redwankhan144@gmail.com>
2023-04-10 23:42:45 -0400

    Updated homepage and about page

--- src/components/about.js
+++ src/components/about.js
@@ -10,16 +10,20 @@ export default function About() {
         {/* //About Us title */}
         <h1 style={{fontSize: '3.5rem', color:'#fff', fontWeight: '300'}}>About Us</h1>
 
-
-
 <h2 style={{fontSize: '1.5em'}}>
-        Rawdah Institute is an educational institute that strives to connect Muslims of all walks of life with the Islamic tradition.
+        Rawdah Institute is an educational institute that strives to connect Muslims of all walks of life with the Islamic tradition. Rawdah Institute aims to provide high quality instruction on content that includes: Quranic Recitation, Arabic Language, Islamic Studies, and Self-Development.
+
 </h2>
 
+<h6 style={{fontSize: '3.5rem', color:'#fff', fontWeight: '300'}}>Our Mission</h6>
+
+<h5 style={{fontSize: '1.5em'}}>
+To inspire people with the truth and to empower them with knowledge that will lead to action
+</h5>
 
 
 <h3 style={{fontSize: "25px", padding:'7px 10px', borderRadius: '5px'}}>
-Rawdah Institute aims to provide high quality instruction on content that includes: Quranic Recitation, Arabic Language, Islamic Studies, and Self-Development.
+{/* Rawdah Institute aims to provide high quality instruction on content that includes: Quranic Recitation, Arabic Language, Islamic Studies, and Self-Development. */}
 
 </h3>
 </div>
@@ -29,3 +33,7 @@ Rawdah Institute aims to provide high quality instruction on content that includ
     ) 
     
 }
+
+
+
+
