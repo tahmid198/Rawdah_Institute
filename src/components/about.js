05786eaf8f0b108212479edd34a22d68f508a7df
redwankhan <redwankhan144@gmail.com>
2023-04-14 02:06:50 -0400

    updated about page text and text alignment

--- src/components/about.js
+++ src/components/about.js
@@ -1,34 +1,60 @@
 import './css/about.css';
 export default function About() {
     return(
-    <section className="about-container">
 
-        <div className='about'>
-        
-        <div className='about-info'>
-        
-        {/* //About Us title */}
-        <h1 style={{fontSize: '3.5rem', color:'#fff', fontWeight: '300'}}>About Us</h1>
+<section className="about-container">
+  <div className="about">
+    <div className="about-info">
+      {/* About Us title */}
+      <h1 style={{ fontSize: "3.5rem", color: "#fff", fontWeight: "300" }}>
+        About Us
+      </h1>
 
-<h2 style={{fontSize: '1.5em'}}>
-        Rawdah Institute is an educational institute that strives to connect Muslims of all walks of life with the Islamic tradition. Rawdah Institute aims to provide high quality instruction on content that includes: Quranic Recitation, Arabic Language, Islamic Studies, and Self-Development.
+      {/* Rawdah Institute description */}
+      <p className = "about-desc" 
+      style={{ fontSize: "1.5em", color: 'white' }}>
+        Rawdah Institute is an educational institute that strives to connect
+        Muslims of all walks of life with the Islamic tradition. Rawdah
+        Institute aims to provide high quality instruction on content that
+        includes: Quranic Recitation, Arabic Language, Islamic Studies, and
+        Self-Development.
+      </p>
 
-</h2>
+      {/* Our Mission title */}
+      <h10
+        className="ourMissionTitle"
+        style={{ fontSize: "3.5rem", color: "#fff", fontWeight: "300" }}
+      >
+        Our Mission
+      </h10>
 
-<h6 style={{fontSize: '3.5rem', color:'#fff', fontWeight: '300'}}>Our Mission</h6>
+      {/* Our Mission description */}
+      <p
+        className="ourMissionDesc"
+        style={{ fontSize: "1.5em", height: 30, color: "#fff" }}
+      >
+        To inspire people with the truth and to empower them with knowledge
+        that will lead to action.
+      </p>
 
-<h5 style={{fontSize: '1.5em'}}>
-To inspire people with the truth and to empower them with knowledge that will lead to action
-</h5>
+      {/* Our Vision title */}
+      <h6
+        className="ourVision"
+        style={{ fontSize: "3.5rem", color: "#fff", fontWeight: "300" }}
+      >
+        Our Vision
+      </h6>
 
-
-<h3 style={{fontSize: "25px", padding:'7px 10px', borderRadius: '5px'}}>
-{/* Rawdah Institute aims to provide high quality instruction on content that includes: Quranic Recitation, Arabic Language, Islamic Studies, and Self-Development. */}
-
-</h3>
-</div>
-</div>
+      {/* Our Vision description */}
+      <p className="ourVision-desc" style={{ fontSize: "1.5em", height: 30 }}>
+      Our vision for Rawdah Institute is for it to become a vibrant, state-of-the-art, world-class institute that will provide rich and accessible content on the Islamic tradition for the everyday Muslim.
+      </p>
+    </div>
+  </div>
 </section>
+
+
+
            
     ) 
     
