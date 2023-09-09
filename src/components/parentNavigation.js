d1fc83ad93e040bf91140043428cad5dea2cf5bf
Tahmid Zaman <tahmid198@yahoo.com>
2023-09-09 13:02:07 -0400

    cleaning files

--- src/components/parentNavigation.js
+++ src/components/parentNavigation.js
@@ -16,29 +16,6 @@ const ParentNavigation = () => {
     setHamburgerOpen(!hamburgerOpen)
   }
 
-    // // Use a ref to get a reference to the sidebar element
-    // const sidebarRef = useRef(null);
-
-    // // Use the useEffect hook to handle outside click events
-    // useEffect(() => {
-    //   const handleOutsideClick = (event) => {
-    //     // Check if the click target is outside the sidebar menus
-    //     if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
-    //       toggleHamburger(); // Call toggleHamburger to update the state
-    //     }
-    //   }
-  
-    //   // Add the event listener on mount
-    //   window.addEventListener('click', handleOutsideClick);
-  
-    //   // Remove the event listener on unmount
-    //   return () => {
-    //     window.removeEventListener('click', handleOutsideClick);
-    //   }
-    // }, [isOpen, hamburgerOpen]); // Update the dependency array to include isOpen and hamburgerOpen
-  
-
-
   return (
     <div>
       
