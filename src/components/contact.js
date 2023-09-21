27ce2bba8a74facee0ade332c64dc55a10f0995c
Tahmid Zaman <tahmid198@yahoo.com>
2023-09-21 01:06:53 -0400

    updating contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,10 +10,10 @@ export default function Contact() {
     e.preventDefault();
     emailjs
       .sendForm(
-        process.env.secrets.REACT_APP_SERVICE_ID,
-        process.env.secrets.REACT_APP_TEMPLATE_ID,
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
         form.current,
-        process.env.secrets.REACT_APP_PUBLIC_KEY
+        process.env.REACT_APP_PUBLIC_KEY
       )
       .then(
         (result) => {
